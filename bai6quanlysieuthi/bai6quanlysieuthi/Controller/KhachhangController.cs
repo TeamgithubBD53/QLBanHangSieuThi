22ec15d57caa8f5ad813865fd392f0c737d04244
unknown <khanhnguyenk8@gmail.com>
2020-11-27 13:36:44 +0700

    Start
    Tạo CSDL và khung chương trình

---
+++ bai6quanlysieuthi/bai6quanlysieuthi/Controller/KhachhangController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bai6quanlysieuthi.Models;
+using System.Data;
+using bai6quanlysieuthi.Bus;
+namespace bai6quanlysieuthi.Controller
+{
+    class KhachhangController
+    {
+       
+    }
+}
